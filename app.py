160b6fd4106ffcd1993f6e377233b944031d0c05
Sharan Kalyan <sharandhoni2526@gmail.com>
2026-01-05 17:03:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def add_bg_from_local(image_file):
         unsafe_allow_html=True
     )
 
-add_bg_from_local("Banking.png")
+add_bg_from_local("CreditCheck.jpg")
 
 # --------------------------------------------------
 # Title & Hero Section
@@ -273,6 +273,7 @@ if uploaded_file:
         "credit_predictions.csv",
         "text/csv"
     )
+
 
 
 
